7582d105a6c802255251fd0b7397820c4b61bf75
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2022-04-20 23:07:11 -0400

    added confetti

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Home.css';
-import { carrot, github, viewer } from '../images';
+import { carrot, github, viewer, anime } from '../images';
+import { startConfetti } from '../confetti';
 
 //https://blaze-comic-viewer.netlify.app
 //https://hershnagpal.github.io/carrot-client/
 
 const Home = () => {
     const [tab, setTab] = useState('none');
+
+    useEffect(() => {
+        startConfetti();
+    }, []);
 
     const slide = (id) => {
         const linksContainer = document.getElementById('linksContainer');
@@ -27,21 +32,27 @@ const Home = () => {
     };
 
     return <>
-        <div className="homeContainer" id="detailsContainer">
-            <span>{tab}</span>
-        </div>
-        <div className="homeContainer" id="linksContainer">
-            <div className="linkBox" id="viewerBox" style={tab === 'viewer' ? {background: 'cornflowerblue'} : {}} onClick={() => slide('viewer')}>
-                <img src={viewer} width="20" height="20" alt="viewer" />
-                <span> Blaze Comic Viewer</span>
+        <div className="outerContainer">
+            <img className="animeImage" src={anime} alt="anime" />
+            <span className="titleText" style={{color: 'rgb(7,255,234)', left: '55px', top: '52px'}}>水を飲む</span>
+            <span className="titleText">水を飲む</span>
+            <span className="titleText" style={{color: 'black', left: '55px'}}>水を飲む</span>
+            <div className="homeContainer" id="detailsContainer">
+                <span>{tab}</span>
             </div>
-            <div className="linkBox" id="carrotBox" style={tab === 'carrot' ? {background: 'cornflowerblue'} : {}} onClick={() => slide('carrot')}>
-                <img src={carrot} width="20" height="20" alt="carrot" />
-                <span> CarrotWolf</span>
-            </div>
-            <div className="linkBox" id="githubBox" style={tab === 'github' ? {background: 'cornflowerblue'} : {}} onClick={() => slide('github')}>
-                <img src={github} width="20" height="20" alt="github" />
-                <span> GitHub</span>
+            <div className="homeContainer" id="linksContainer">
+                <div className="linkBox" id="viewerBox" style={tab === 'viewer' ? {background: 'rgb(7,255,234)'} : {}} onClick={() => slide('viewer')}>
+                    <img src={viewer} width="20" height="20" alt="viewer" />
+                    <span> Blaze Comic Viewer</span>
+                </div>
+                <div className="linkBox" id="carrotBox" style={tab === 'carrot' ? {background: 'rgb(7,255,234)'} : {}} onClick={() => slide('carrot')}>
+                    <img src={carrot} width="20" height="20" alt="carrot" />
+                    <span> CarrotWolf</span>
+                </div>
+                <div className="linkBox" id="githubBox" style={tab === 'github' ? {background: 'rgb(7,255,234)'} : {}} onClick={() => slide('github')}>
+                    <img src={github} width="20" height="20" alt="github" />
+                    <span> GitHub</span>
+                </div>
             </div>
         </div>
     </>
--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,4 @@
 export const carrot = require('./carrot.png');
 export const github = require('./github.png');
 export const viewer = require('./viewer.png');
+export const anime = require('./anime.jpg');
