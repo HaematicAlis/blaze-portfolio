df82ff347aebe33be7ebde8f5a53ec22371558d2
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2022-04-20 23:25:53 -0400

    added tree image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './Home.css';
-import { carrot, github, viewer, anime } from '../images';
+import { carrot, github, viewer, anime, tree } from '../images';
 import { startConfetti } from '../confetti';
 
 //https://blaze-comic-viewer.netlify.app
@@ -31,14 +31,26 @@ const Home = () => {
         }
     };
 
+    const getContent = () => {
+        switch (tab) {
+            case 'viewer':
+                return <>
+                    <span>Viewer</span>
+                </>;
+            default:
+                break;
+        }
+    };
+
     return <>
         <div className="outerContainer">
             <img className="animeImage" src={anime} alt="anime" />
+            <img className="treeImage" src={tree} alt="tree" />
             <span className="titleText" style={{color: 'rgb(7,255,234)', left: '55px', top: '52px'}}>水を飲む</span>
             <span className="titleText">水を飲む</span>
             <span className="titleText" style={{color: 'black', left: '55px'}}>水を飲む</span>
             <div className="homeContainer" id="detailsContainer">
-                <span>{tab}</span>
+                {getContent}
             </div>
             <div className="homeContainer" id="linksContainer">
                 <div className="linkBox" id="viewerBox" style={tab === 'viewer' ? {background: 'rgb(7,255,234)'} : {}} onClick={() => slide('viewer')}>
--- src/images/index.js
+++ src/images/index.js
@@ -2,3 +2,4 @@ export const carrot = require('./carrot.png');
 export const github = require('./github.png');
 export const viewer = require('./viewer.png');
 export const anime = require('./anime.jpg');
+export const tree = require('./tree.png');
