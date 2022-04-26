84ecaa300c290e69a01b7625ba5906ded2332a4e
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2022-04-26 18:02:34 -0400

    changed links to open in new tab

--- src/components/Home.js
+++ src/components/Home.js
@@ -59,7 +59,7 @@ const Home = () => {
                         <span>·	Full vocab list and comic completion can be viewed from the shelf</span><br />
                         <span>·	Integrated dictionary search and zoom settings</span><br />
                         <span><b>· React, MERN, JavaScript, MongoDB, ExpressJS, NodeJS, HTML, CSS, Redux, Material UI</b></span><br />
-                        <a href="https://blaze-comic-viewer.netlify.app">Demo it here with Username: Freeze, Password: 123</a>
+                        <a href="https://blaze-comic-viewer.netlify.app" target="_blank" rel="noreferrer">Demo it here with Username: Freeze, Password: 123</a>
                     </>
 
                     : tab === '7red3'
@@ -73,7 +73,7 @@ const Home = () => {
                         <span>·	Motion work shows Vimeo popups and design work shows image collections</span><br />
                         <span>·	Admin dashboard for uploading and managing the work in the database</span><br />
                         <span><b>· React, JS, HTML, CSS, MySQL, PHP, GoDaddy</b></span><br /><br />
-                        <a href="https://7red3.com">Check it out here!</a>
+                        <a href="https://7red3.com" target="_blank" rel="noreferrer">Check it out here!</a>
 
                     </>
 
@@ -89,7 +89,7 @@ const Home = () => {
                         <span>·	Styled with Material UI and custom components</span><br />
                         <span>·	Features enemy AI and random collectable items</span><br />
                         <span><b>· React, JS, HTML, CSS, Redux, Material UI</b></span><br />
-                        <a href="http://hershnagpal.github.io/carrot-client/">Play the game here!</a>
+                        <a href="http://hershnagpal.github.io/carrot-client/" target="_blank" rel="noreferrer">Play the game here!</a>
 
                     </>
 
@@ -100,25 +100,15 @@ const Home = () => {
                         <hr />
                         <span>Currently working on:</span><br />
                         <span><b>Portfolio website</b></span><br /><br />
-                        <a href="https://github.com/HaematicAlis">Click here for my GitHub!</a>
+                        <a href="https://github.com/HaematicAlis" target="_blank" rel="noreferrer">Click here for my GitHub!</a>
                     </>
 
                     : tab === 'credits'
                     ? <>
                         <span>Confetti particle script: </span>
-                        <a href="https://www.cssscript.com/confetti-falling-animation/">CSS Script</a><br />
+                        <a href="https://www.cssscript.com/confetti-falling-animation/" target="_blank" rel="noreferrer">CSS Script</a><br />
                         <span>Font: </span>
-                        <a href="https://fonts.google.com/specimen/Kosugi+Maru">Kosugi Maru</a><br />
-                        <span>Made with: React</span><br />
-                        <span>Image by: me</span>
-                    </>
-
-                    : tab === 'resume'
-                    ? <>
-                        <span>Confetti particle script: </span>
-                        <a href="https://www.cssscript.com/confetti-falling-animation/">CSS Script</a><br />
-                        <span>Font: </span>
-                        <a href="https://fonts.google.com/specimen/Kosugi+Maru">Kosugi Maru</a><br />
+                        <a href="https://fonts.google.com/specimen/Kosugi+Maru" target="_blank" rel="noreferrer">Kosugi Maru</a><br />
                         <span>Made with: React</span><br />
                         <span>Image by: me</span>
                     </>
@@ -147,7 +137,7 @@ const Home = () => {
                     <img src={credits} width="20" height="20" alt="credits" />
                     <span> Credits</span>
                 </div>
-                <a href="http://blazewiseman.com/files/resume.pdf" download style={{color: 'black'}}>
+                <a href="http://blazewiseman.com/files/resume.pdf" target="_blank" rel="noreferrer" style={{color: 'black'}}>
                     <div className="linkBox" id="resumeBox" style={tab === 'resume' ? {background: 'rgb(7,255,234)'} : {}}>
                         <img src={downarrow} width="20" height="20" alt="downarrow" />
                         <span> Resume</span>
