afcec28b260f3c1c0169a56b46cf0f00c43beeb6
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2022-04-29 17:36:30 -0400

    responsiveness round 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,9 +34,9 @@ const Home = () => {
             case 'viewer':
                 return 'Blaze Comic Viewer';
             case '7red3':
-                return '7Red3 Motion Graphics Studio';
+                return '7Red3';
             case 'carrot':
-                return 'CarrotWolf - The Wolf Who Cried Carrot';
+                return 'CarrotWolf';
             case 'github':
                 return 'GitHub';
             case 'credits':
@@ -63,7 +63,7 @@ const Home = () => {
             <span className="titleText" style={{color: 'black', right: '25px'}}>Blaze Wiseman</span>
             <div className="homeContainer" id="detailsContainer">
             <div className="homeBar">
-                <span style={{position: 'absolute', top: '4px'}}>&nbsp;* {getName()}</span>
+                <span className="homeBarText">&nbsp;* {getName()}</span>
                 <span className="homeIcon" onClick={() => slide(tab)}>
                     <img className="backarrowImage" src={backarrow} alt="backarrow" />
                 </span>
@@ -71,48 +71,60 @@ const Home = () => {
                 {
                     tab === 'viewer'
                     ? <>
-                        <img className="thumbnailImage" src={comicviewer} alt="comicviewer" onClick={() => setPopup({ visible: true, image: comicviewer })} />
+                        <div className="thumbnailBox">
+                            <img className="thumbnailImage" src={comicviewer} alt="comicviewer" onClick={() => setPopup({ visible: true, image: comicviewer })} />
+                        </div>
                         <img className="magnifyImage" src={magnify} alt="magnify" onClick={() => setPopup({ visible: true, image: comicviewer })} />
                         <hr />
-                        <span>·	Comic viewer website for language learning</span><br />
-                        <span>· Made with React and MERN stack on Netlify</span><br />
-                        <span>·	Uses Imgur API to upload albums as comics</span><br />
-                        <span>·	Vocab can be added to each page individually</span><br />
-                        <span>· Shelf to view, sort, and manage comics and books</span><br />
-                        <span>·	Full vocab list and completion % can be viewed</span><br />
-                        <span>·	Integrated dictionary search and zoom settings</span><br />
-                        <span><b>· React, MERN, JavaScript, MongoDB, ExpressJS,</b></span><br />
-                        <span><b>&nbsp;&nbsp;NodeJS, HTML, CSS, Redux, Material UI</b></span><br />
-                        <a className="bottomText" href="https://blaze-comic-viewer.netlify.app" target="_blank" rel="noreferrer">Demo it here with Username: Freeze, Password: 123</a>
+                        <div className="textContainer">
+                            <span>·	Comic viewer website for language learning</span><br />
+                            <span>· Made with React and MERN stack on Netlify</span><br />
+                            <span>·	Uses Imgur API to upload albums as comics</span><br />
+                            <span>·	Vocab can be added to each page individually</span><br />
+                            <span>· Shelf to view, sort, and manage comics and books</span><br />
+                            <span>·	Full vocab list and completion % can be viewed</span><br />
+                            <span>·	Integrated dictionary search and zoom settings</span><br />
+                            <span><b>· React, MERN, JavaScript, MongoDB, ExpressJS,</b></span><br />
+                            <span><b>&nbsp;&nbsp;NodeJS, HTML, CSS, Redux, Material UI</b></span><br />
+                        </div>
+                        <a className="bottomText" href="https://blaze-comic-viewer.netlify.app" target="_blank" rel="noreferrer">Demo User:Freeze, Pass:123</a>
                     </>
 
                     : tab === '7red3'
                     ? <>
-                        <img className="thumbnailImage" src={red} alt="red" onClick={() => setPopup({ visible: true, image: red })} />
+                        <div className="thumbnailBox">
+                            <img className="thumbnailImage" src={red} alt="red" onClick={() => setPopup({ visible: true, image: red })} />
+                        </div>
                         <img className="magnifyImage" src={magnify} alt="magnify" onClick={() => setPopup({ visible: true, image: red })} />
                         <hr />
-                        <span>·	Company website for 7Red3</span><br />
-                        <span>·	Used as a portfolio of motion graphics work</span><br />
-                        <span>·	Motion work shows Vimeo popups</span><br />
-                        <span>· Design work shows image collections</span><br />
-                        <span>·	Admin dashboard for uploading and managing</span><br />
-                        <span>·	Admin can edit and reorder work in the database</span><br />
-                        <span><b>· React, JS, HTML, CSS, MySQL, PHP, GoDaddy</b></span><br /><br />
+                        <div className="textContainer">
+                            <span>·	Company website for 7Red3</span><br />
+                            <span>·	Used as a portfolio of motion graphics work</span><br />
+                            <span>·	Motion work shows Vimeo popups</span><br />
+                            <span>· Design work shows image collections</span><br />
+                            <span>·	Admin dashboard for uploading and managing</span><br />
+                            <span>·	Admin can edit and reorder work in the database</span><br />
+                            <span><b>· React, JS, HTML, CSS, MySQL, PHP, GoDaddy</b></span><br /><br />
+                        </div>
                         <a className="bottomText" href="https://7red3.com" target="_blank" rel="noreferrer">Check it out here!</a>
 
                     </>
 
                     : tab === 'carrot'
                     ? <>
-                        <img className="thumbnailImage" src={carrotwolf} alt="carrotwolf" onClick={() => setPopup({ visible: true, image: carrotwolf })} />
+                        <div className="thumbnailBox">
+                            <img className="thumbnailImage" src={carrotwolf} alt="carrotwolf" onClick={() => setPopup({ visible: true, image: carrotwolf })} />
+                        </div>
                         <img className="magnifyImage" src={magnify} alt="magnify" onClick={() => setPopup({ visible: true, image: carrotwolf })} />
                         <hr />
-                        <span>·	Turn-based strategy browser game made with React</span><br />
-                        <span>·	Used Redux for keeping track of the board state</span><br />
-                        <span>·	Controlled using keyboard input and the mouse</span><br />
-                        <span>·	Styled with Material UI and custom components</span><br />
-                        <span>·	Features enemy AI and random collectable items</span><br />
-                        <span><b>· React, JS, HTML, CSS, Redux, Material UI</b></span><br />
+                        <div className="textContainer">
+                            <span>·	Turn-based strategy browser game made with React</span><br />
+                            <span>·	Used Redux for keeping track of the board state</span><br />
+                            <span>·	Controlled using keyboard input and the mouse</span><br />
+                            <span>·	Styled with Material UI and custom components</span><br />
+                            <span>·	Features enemy AI and random collectable items</span><br />
+                            <span><b>· React, JS, HTML, CSS, Redux, Material UI</b></span><br />
+                        </div>
                         <a className="bottomText" href="http://hershnagpal.github.io/carrot-client/" target="_blank" rel="noreferrer">Play the game here!</a>
 
                     </>
@@ -142,33 +154,33 @@ const Home = () => {
             </div>
             <div className="homeContainer" id="linksContainer">
                 <div className="homeBar">
-                    <span style={{position: 'absolute', top: '4px'}}>&nbsp;*    Local Disc (C:/{tab === 'none' ? '' : tab})</span>
+                    <span className="homeBarText">&nbsp;*    Local Disc (C:/{tab === 'none' ? '' : tab})</span>
                     <span className="homeIcon">X</span>
                 </div>
                 <div className="linkBox" id="viewerBox" style={tab === 'viewer' ? {background: 'rgb(7,255,234)'} : {}} onClick={() => slide('viewer')}>
-                    <img src={viewer} width="20" height="20" alt="viewer" />
-                    <span> Blaze Comic Viewer</span>
+                    <img className="linkBoxIcon" src={viewer} alt="viewer" />
+                    <span className="linkBoxText"> Blaze Comic Viewer</span>
                 </div>
                 <div className="linkBox" id="7red3box" style={tab === '7red3' ? {background: 'rgb(7,255,234)'} : {}} onClick={() => slide('7red3')}>
-                    <img src={seventhree} width="20" height="20" alt="seventhree" />
-                    <span> 7Red3</span>
+                    <img className="linkBoxIcon" src={seventhree} alt="seventhree" />
+                    <span className="linkBoxText"> 7Red3</span>
                 </div>
                 <div className="linkBox" id="carrotBox" style={tab === 'carrot' ? {background: 'rgb(7,255,234)'} : {}} onClick={() => slide('carrot')}>
-                    <img src={carrot} width="20" height="20" alt="carrot" />
-                    <span> CarrotWolf</span>
+                    <img className="linkBoxIcon" src={carrot} alt="carrot" />
+                    <span className="linkBoxText"> CarrotWolf</span>
                 </div>
                 <div className="linkBox" id="githubBox" style={tab === 'github' ? {background: 'rgb(7,255,234)'} : {}} onClick={() => slide('github')}>
-                    <img src={github} width="20" height="20" alt="github" />
-                    <span> GitHub</span>
+                    <img className="linkBoxIcon" src={github} alt="github" />
+                    <span className="linkBoxText"> GitHub</span>
                 </div>
                 <div className="linkBox" id="creditsBox" style={tab === 'credits' ? {background: 'rgb(7,255,234)'} : {}} onClick={() => slide('credits')}>
-                    <img src={credits} width="20" height="20" alt="credits" />
-                    <span> Credits</span>
+                    <img className="linkBoxIcon" src={credits} alt="credits" />
+                    <span className="linkBoxText"> Credits</span>
                 </div>
                 <a href="http://blazewiseman.com/files/resume.pdf" target="_blank" rel="noreferrer" style={{color: 'black'}}>
                     <div className="linkBox" id="resumeBox" style={tab === 'resume' ? {background: 'rgb(7,255,234)'} : {}}>
-                        <img src={downarrow} width="20" height="20" alt="downarrow" />
-                        <span> Resume</span>
+                        <img className="linkBoxIcon" src={downarrow} alt="downarrow" />
+                        <span className="linkBoxText"> Resume</span>
                     </div>
                 </a>
             </div>
